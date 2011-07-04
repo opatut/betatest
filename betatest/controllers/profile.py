136c32909d4fc3374c8ef10e2726b1759217516c
zetaron <zetaron@live.com>
2011-07-05 00:08:28 +0200

    Added templates for Profile and single Project show mode.

--- betatest/controllers/profile.py
+++ betatest/controllers/profile.py
@@ -3,4 +3,5 @@ from betatest import *
 @app.route("/<username>")
 def profile(username):
 	user = models.user.User.query.filter_by(username = username).first_or_404()
-	return render_template("home.html", message = "user found")
+	
+	return render_template("profile.html", user = user)
