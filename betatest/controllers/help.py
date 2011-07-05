d6b6c67ca31ff9b8b7e22ebd0a61b4dc2c165bb7
opatut <opatutlol@aol.com>
2011-07-06 01:24:28 +0200

    Removed trailing slashes from help URLs

--- betatest/controllers/help.py
+++ betatest/controllers/help.py
@@ -1,7 +1,7 @@
 from betatest import *
 
-@app.route("/about/")
-@app.route("/help/")
+@app.route("/about")
+@app.route("/help")
 @app.route("/help/<path:page>")
 def help(page = 'about'):
 	return "about: " + page
