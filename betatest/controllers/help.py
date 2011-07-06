a108419258aa0ee1c122151e773416e809ddcaac
opatut <opatutlol@aol.com>
2011-07-06 04:00:22 +0200

    Added about page system parsing markdown from files in betatest/pages

--- betatest/controllers/help.py
+++ betatest/controllers/help.py
@@ -1,7 +1,20 @@
 from betatest import *
+import os, sys
+
+about_pages = {
+	"about" 	: "About",
+	"contact" 	: "Contact",
+	"terms-of-service" : "Terms of Service",
+	"privacy"	: "Privacy",
+}
 
 @app.route("/about")
 @app.route("/help")
 @app.route("/help/<path:page>")
 def help(page = 'about'):
-	return "about: " + page
+	if page in about_pages:
+		filename = sys.path[0] + "/betatest/pages/" + page + ".md"
+		md = file.read(open(filename , "r"))
+		return render_template("static.html", title = about_pages[page], content = md)
+	else:
+		abort(404)
