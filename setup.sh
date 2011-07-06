a5cc1ff694f066b822a9f72b2170da6c48d564cb
opatut <opatutlol@aol.com>
2011-07-06 03:06:36 +0200

    Added setup script.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+virtualenv -p python2 env
+. env/bin/activate
+pip install flask
+pip install flask-sqlalchemy
+pip install flask-wtf
+pip install flask-markdown
+pip install blinker
+pip install flask-login
