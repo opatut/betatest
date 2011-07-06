615ad6b7814aefa3300d03e3b1fc0f1696c6b908
opatut <opatutlol@aol.com>
2011-07-06 19:38:23 +0200

    Fixed tag ajax

--- betatest/controllers/ajax.py
+++ betatest/controllers/ajax.py
@@ -1,14 +1,13 @@
 from betatest import *
 
-@app.route("/ajax/add_project_tag/<slug>/<tag>")
-def ajax_add_project_tag(slug, tag):
-    project = models.project.Project.query.filter_by(slug = slug).first()
+@app.route("/ajax/add_project_tag/<int:project_id>/<tag>")
+def ajax_add_project_tag(project_id, tag):
+    project = models.project.Project.query.filter_by(id = project_id).first()
     for tag in re.split("\s*,\s*", tag):
-		t = models.tag.Tag.getTag(tag.strip())
-		if not t in project.tags:
-			project.tags.append(t)
-		db.session.commit()
-	
+        t = models.tag.Tag.getTag(tag.strip())
+        if not t in project.tags:
+            project.tags.append(t)
+    db.session.commit()
     return render_template("tags_list.html", tags = project.tags, delete_tag_endpoint = 'ajax_project_tags_remove')
 
 @app.route("/ajax/add_user_tag/<tag>")
@@ -19,4 +18,23 @@ def ajax_add_user_tag(tag):
         if not t in user.tags:
             user.tags.append(t)
     db.session.commit()
+    return render_template("tags_list.html", tags = user.tags, delete_tag_endpoint = 'ajax_settings_tags_remove')
+
+
+@app.route("/ajax/remove_project_tag/<int:project_id>/<tag>")
+def ajax_remove_project_tag(project_id, tag):
+    project = models.project.Project.query.filter_by(id = project_id).first()
+    t = models.tag.Tag.getTag(tag.strip())
+    if t in project.tags:
+        t.projects.remove(project)
+        db.session.commit()
+    return render_template("tags_list.html", tags = project.tags, delete_tag_endpoint = 'ajax_project_tags_remove')
+
+@app.route("/ajax/remove_user_tag/<tag>")
+def ajax_remove_user_tag(tag):
+    user = usersession.getCurrentUser()
+    t = models.tag.Tag.getTag(tag.strip())
+    if t in user.tags:
+        t.users.remove(user)
+        db.session.commit()
     return render_template("tags_list.html", tags = user.tags, delete_tag_endpoint = 'ajax_settings_tags_remove')
--- betatest/static/tags.js
+++ betatest/static/tags.js
@@ -1,27 +1,81 @@
+$.fn.tagsDeleteButtons = function() {
+	var spinner = $(this).find('.icon-16-spinner');
+	var form = $(this).closest("form");
+	var list = $(this);
+	
+	$(this).find("li").each(function() {
+		$(this).find("a.delete").click(function() {
+			var tag = $(this).parent().find(".tag").text();
+			var href = $(this).attr("href");
+			var project_id = form.attr("project_id");		
+			var url_ = $ROOT_PATH;
+			if(project_id) {
+				url_ += "/ajax/remove_project_tag/" + project_id + "/" + tag;
+			} else {
+				url_ += "/ajax/remove_user_tag/" + tag;
+			}
+			
+			$.ajax({
+				url: url_,
+				success: function(data){
+					new_list = $(data);
+					list.replaceWith(new_list);
+					new_list.tagsDeleteButtons();
+					spinner.fadeOut(200);
+				},
+				error: function() {
+					alert("Ajax error. Redirecting as fallback.");
+					window.location = href;
+				}
+			});
+			return false;
+		});
+	});
+};
+
 $.fn.tags = function() {
+	var list = $(this);
+	
 	$(this).closest('form').submit(function() {
-		var value = $(this).closest('form').find('.input-text').val();
-		var spinner = $(this).closest('p').next('p').find('span');
+		var form = $(this);
+		if (form.attr("ajaxerror") == "true") {
+			form.attr("ajaxerror", "false");
+			return true;
+		}
 		
-		var slug = $(this).attr('slug');
-		var project_url = "/ajax/add_project_tag/"+slug+"/"+value;
-		var url_ = "/ajax/add_user_tag/"+value;
-		if(slug)
-			url_ = project_url;
+		var value = $(this).find('.input-text').val();
+		var spinner = $(this).find('.icon-16-spinner');
 		
-		spinner.css('display', 'block');	
+		var project_id = $(this).attr("project_id");
 		
+		var url_ = $ROOT_PATH;
+		if(project_id) {
+			url_ += "/ajax/add_project_tag/" + project_id + "/" + value;
+		} else {
+			url_ += "/ajax/add_user_tag/" + value;
+		}
+		
+		spinner.fadeIn(200);
 		$.ajax({
-			url: $ROOT_PATH + url_,
+			url: url_,
 			success: function(data){
-				$(this).find('.tags').replaceWith(data);
+				new_list = $(data);
+				list.replaceWith(new_list);
+				new_list.tagsDeleteButtons();
+				spinner.fadeOut(200);
+			},
+			error: function() {
+				alert("Ajax error. Submitting form as fallback.");
+				form.attr("ajaxerror", "true");
+				form.submit()
 			}
 		});
 		
-		spinner.css('display', 'none');
+		$(this).find('.input-text').val("");
 		
 		return false;
 	});
+	list.tagsDeleteButtons();
 };
 
 $(document).ready(function() {
