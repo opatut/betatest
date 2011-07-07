5a01e7d8ad0ea10977b7686f6b722ba0171ac8ec
opatut <opatutlol@aol.com>
2011-07-07 14:42:48 +0200

    Added favicon

--- betatest/static/messages.js
+++ betatest/static/messages.js
@@ -1,65 +1,64 @@
 // message list selection
 
 function formAllSelected(form) {
-	return form.find("input.selection").length == form.find("input.selection:checked").length;
+    return form.find("input.selection").length == form.find("input.selection:checked").length;
 }
 
 function selectAllCheck(form) {
-	if(formAllSelected(form)) {
-		form.find(".select_all").val("Unselect all");
-	} else {
-		form.find(".select_all").val("Select all");
-	}
+    if(formAllSelected(form)) {
+        form.find(".select_all").val("Unselect all");
+    } else {
+        form.find(".select_all").val("Select all");
+    }
 }
 
 function toggleSelectAll(form) {
-	new_v = !formAllSelected(form);
-	form.find("input.selection").each(function() {
-		$(this).attr("checked", new_v);
-		toggleSelected($(this));
-	});
+    new_v = !formAllSelected(form);
+    form.find("input.selection").each(function() {
+        $(this).attr("checked", new_v);
+        toggleSelected($(this));
+    });
 }
 
 function toggleSelected(input) {
-	if(input.attr("checked")) {
-		input.closest("li").addClass("selected");
-	} else {
-		input.closest("li").removeClass("selected");
-	}
-	selectAllCheck(input.closest("form"));
+    if(input.attr("checked")) {
+        input.closest("li").addClass("selected");
+    } else {
+        input.closest("li").removeClass("selected");
+    }
+    selectAllCheck(input.closest("form"));
 }
 
 $(document).ready(function() {
-	$("ul.message-list-ul li").click(function() {
-		var $c = $(this).find("input.selection");
-		$c.attr("checked", !$c.attr("checked"));
-		toggleSelected($c);
-	}).dblclick(function() {
-		window.location.href = $(this).find("a.message_title").attr("href");
-	});
-	
-	$("ul.message-list-ul li input.selection").click(function() {
-		$(this).attr("checked", !$(this).attr("checked"));
-		toggleSelected($(this));
-	});
-	
-	$("ul.message-list-ul li input.selection").each(function() {
-		toggleSelected($(this));
-	});
-	
-	$(".select_all").click(function() { 
-		toggleSelectAll($(this).closest("form")); 
-	})
-	
-	$(".no-js-hide").css("visibility","visible");
+    $("ul.message-list-ul li").click(function() {
+        var $c = $(this).find("input.selection");
+        $c.attr("checked", !$c.attr("checked"));
+        toggleSelected($c);
+    }).dblclick(function() {
+        window.location.href = $(this).find("a.message_title").attr("href");
+    });
 
-	$('.user-autocomplete').autocomplete({ 
-		serviceUrl:'/ajax/users/autocomplete',
-		minChars: 1, 
-		// delimiter: /(,|;)\s*/,
-		maxHeight: 200,
-		zIndex: 9999,
-		noCache: false
-	});
-	
+    $("ul.message-list-ul li input.selection").click(function() {
+        $(this).attr("checked", !$(this).attr("checked"));
+        toggleSelected($(this));
+    });
+
+    $("ul.message-list-ul li input.selection").each(function() {
+        toggleSelected($(this));
+    });
+
+    $(".select_all").click(function() {
+        toggleSelectAll($(this).closest("form"));
+    })
+
+    $(".no-js-hide").css("visibility","visible");
+
+    $('.user-autocomplete').autocomplete({
+        serviceUrl:'/ajax/users/autocomplete',
+        minChars: 1,
+        // delimiter: /(,|;)\s*/,
+        maxHeight: 200,
+        zIndex: 9999,
+        noCache: false
+    });
 });
